1f61696e4b232d31925eb6ccf329098ac043c4d9
Jelle Verstappen <jelle.verstappen@telenet.be>
2021-11-22 13:51:32 +0100

    P9

--- src/part9.py
+++ src/part9.py
@@ -1,5 +1,15 @@
-class Book(title):
+class Book:
     def __init__(self, title):
         self.title = title
-    
-    
+        self.content = f"Title: {self.title}\n"
+
+    def add_chapter(self, chapter):
+        self.content += f'# {chapter}\n'
+
+    def add_line(self, lines):
+        self.content += f'{lines}\n'
+
+    def write(self, filename):
+        file = open(filename, "wt")
+        file.write(self.content)
+        
