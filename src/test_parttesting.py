9c4762ab2573dd90739e35253041aae3334ceb74
Jelle Verstappen <jelle.verstappen@telenet.be>
2021-11-26 11:19:52 +0100

    update

--- src/test_parttesting.py
+++ src/test_parttesting.py
@@ -1,4 +1,13 @@
 # Schrijf testen voor de functies die je in part_testing.py vindt.
 from .part_testing import bmi, maximum, pythagoras
 
+def test_maximum():
+    result = maximum(0, -1)
+
+    assert result == 0
+
+def test_maximum_2():
+    result = maximum(5, 10)
+
+    assert result == 10
 
