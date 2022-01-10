79b0ec3c020d6c9efcca5f10eaf96a9203a23284
Jelle Verstappen <jelle.verstappen@telenet.be>
2022-01-10 12:17:40 +0100

    update

--- src/part_csv.py
+++ src/part_csv.py
@@ -5,19 +5,21 @@
 # Dit bestand heeft naam "COVID19BE_CASES_AGESEX.csv".
 
 import csv
-import 
+
 def determine_total_cases(age_category, gender):
     """Geeft het totaal aantal COVID19 gevallen terug voor de gegeven leeftijdscategorie en het gegeven geslacht
     B.v.
     determine_total_cases("20-29", "M") geeft het aantal besmettingen van mannen tussen de 20 en 29 jaar oud
     terug. 
     """
-    rows = []
-    f = open("csv_dataset.csv", "r")
+    cases = 0
+    f = open("src/COVID19BE_CASES_AGESEX.csv", "r")
     reader = csv.reader(f, delimiter = ',')
     for row in reader:
-        rows = rows.append[row]
-    print(rows) 
+        if row[3] == age_category and row[4] == gender:
+            cases+= int(row[5])
+
+    return(cases)
         
 
 
@@ -34,6 +36,22 @@ def output_total_cases(input_filename, output_filename):
     ...
     "90+", 123123
     """
-    return 0
+    age1_cases = 0
+    age2_cases = 0
+    age3_cases = 0
+    f = open("src/COVID19BE_CASES_AGESEX.csv", "r")
+    reader = csv.reader(f, delimiter = ',')
 
-determine_total_cases(1-2, "M")
+    f2 = open("src/overzicht.csv", "w")
+    writer = csv.writer(f2)
+    for row in reader:
+        if row[3] == '0-9':
+            age1_cases += int(row[5])
+    writer.writerow(['0-9', age1_cases])
+    
+
+     
+
+
+print(output_total_cases("COVID19BE_CASES_AGESEX.csv", "overzicht.csv"))   
+#print(determine_total_cases("10-19", "M"))
