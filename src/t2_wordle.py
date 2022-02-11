15762dc9bb58140ac2c703437bf93f04bd944ddf
Jelle Verstappen <jelle.verstappen@telenet.be>
2022-02-11 11:55:12 +0100

    einde uur

--- src/t2_wordle.py
+++ src/t2_wordle.py
@@ -7,11 +7,19 @@ from rich import print
 
 def choose_word(words):
     """Een functie die een willekeurig woord uit een lijst kiest"""
+    return words[random.randint(0, len(words))]
+
+        
+
 
 
 def lines_to_words(lines):
     """Een functie die de newlines uit een lijst strings verwijdert"""
-
+    wordlist = []
+    for line in lines:
+        words = line.strip("\n")
+        wordlist.append(words)
+    return wordlist
 
 def process_words(words, length):
     """Een functie die gegeven een lijst woorden de bruikbare woorden filtert
@@ -20,6 +28,24 @@ def process_words(words, length):
     - woorden van de juiste lengte
     - woorden zonder: koppeltekens, spaties, hoofdletters, punten, quotes
     """
+    used = []
+    for word in words:
+        if "-" in word:
+            continue
+        if " " in word:
+            continue
+        if "." in word:
+            continue
+        if '""' in word:
+            continue
+        if word.istitle() in word:
+            continue
+        if word.isupper() in word:
+            continue
+        if len(word) == length:
+            used.append(word)
+    return used
+
 
 
 def combine_letters_of_words(word1, word2):
@@ -32,6 +58,13 @@ def combine_letters_of_words(word1, word2):
     Gebruik hiervoor de zip-functie die je in de standaard bibliotheek
     van Python vindt.
     """
+    listw1 = list(word1)
+    listw2 = list(word2)
+
+    zipped = zip(listw1, listw2)
+    zipped_list = list(zipped)
+
+    return zipped_list
 
 
 def print_letter_volledig_juist(a):
@@ -63,48 +96,48 @@ def print_letter(a, b, secret_word):
     '[red]o[/red]'
     """
 
-if __name__ == "__main__":
+#if __name__ == "__main__":
 
     # lees de woordlijst in
-    filename = "wordlist.txt"
-    f = ...
+    #filename = "wordlist.txt"
+    #f = ...
 
-    lines = ...
+    #lines = ...
 
     # gebruik lines_to_words en
     # process_words om een lijst met
     # woorden van lengte 5 te verkrijgen
-    words = ...
-    words = ...
+    #words = ...
+    #words = ...
 
     # kies een willekeurig woord mbv choose_word
-    secret_word = ...
+    #secret_word = ...
 
-    counter = 1
-    while counter < 6:
+    #counter = 1
+    #while counter < 6:
         # vraag de gebruiker om een gok te wagen
-        w = input("?")
+        #w = input("?")
 
         # als de gebruiker "stop" ingeeft beeindig het programma dan
         # gebruik hiervoor het break statement
-        if ...
+        #if ...
 
         # als het woord geen 5 letters lang is, sla dit woord dan over
         # gebruik hiervoor het continue statement
-        if ...
+        #if ...
 
         # controlleer of het woord correct is, en druk het woord in kleur
         # op het scherm af gebruik makend van combine_letters_of_words en
         # print_letter
-        for a, b in ...
-            letter = ...
-            print(letter, end="")
-        print()
-        counter += 1
+        #for a, b in ...
+        #    letter = ...
+        #    print(letter, end="")
+        #print()
+        #counter += 1
 
         # als het woord correct geraden werd
         # toon dan een overwinningsboodschap
-        if ...
+        #if ...
 
     # toon hoeveel pogingen nodig waren om het woord juist te raden
     ...
